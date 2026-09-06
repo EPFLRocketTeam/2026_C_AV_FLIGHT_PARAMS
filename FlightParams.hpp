e94f4da34ee624913256b1f01acb873ef57e7284
0xPedro123 <166990330+0xPedro123@users.noreply.github.com>
2026-09-06 13:01:03 +0200

    Update FlightParams.hpp for 10% Ullage

--- FlightParams.hpp
+++ FlightParams.hpp
@@ -47,10 +47,10 @@ static_assert(sizeof(IgnitionParams) == 16);
 
 struct BurnParams {
     FIXED   float    PressureIntegralToImpulse = TBD;
-    DYNAMIC uint32_t MinDurationMs             = 4 SECONDS;
+    DYNAMIC uint32_t MinDurationMs             = 4.5 SECONDS;
     DYNAMIC float    Impulse                   = TBD;
     DYNAMIC uint32_t FcMaxDurationMs           = 7 SECONDS;
-    DYNAMIC uint32_t EngineMaxDurationMs       = 4.66 SECONDS;
+    DYNAMIC uint32_t EngineMaxDurationMs       = 4.97 SECONDS;
     DYNAMIC uint32_t CutoffDelayMs             = 500;
 };
 static_assert(sizeof(BurnParams) == 20);
