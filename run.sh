e200419fefe1c7866892a6028a107d47c7415812
theo.hollender <theo.hollender@gmail.com>
2026-09-11 11:03:57 +0200

    main: fix c++ version 20

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-g++ -o build FlightParams.cpp -DCOMMANDS_GENERATOR -I2026_C_AV_CONFMAN/
+g++ -std=c++20 -o build FlightParams.cpp -DCOMMANDS_GENERATOR -I2026_C_AV_CONFMAN/
 ./build | tee params.txt
 
 python3 send_params.py --port $1 --file params.txt --baud 115200 | tee out.txt
