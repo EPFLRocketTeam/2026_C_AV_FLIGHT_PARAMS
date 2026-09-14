99365a73808d7678ccb4c0c57e84ed82143342ff
0xPedro123 <166990330+0xPedro123@users.noreply.github.com>
2026-09-14 16:14:56 +0200

    Update FlightParams.hpp for Coldflow Mischesque Fix

--- FlightParams.hpp
+++ FlightParams.hpp
@@ -22,10 +22,10 @@ struct PressurizationParams {
     DYNAMIC float TargetPressureFuel = -1.f;
 
     /* Ramp Rate (Bar per ms) */
-    DYNAMIC float RampBVOpeningLox  = 33; // 27°
+    DYNAMIC float RampBVOpeningLox  = 44; // 27°
     DYNAMIC float RampBVOpeningFuel = 30; // 27°
 
-    DYNAMIC float StableBVOpeningLox  = 40;
+    DYNAMIC float StableBVOpeningLox  = 54;
     DYNAMIC float StableBVOpeningFuel = 40;
     DYNAMIC float PreburnDurationLoxMs  = 200;
     DYNAMIC float PreburnDurationFuelMs = 200;
